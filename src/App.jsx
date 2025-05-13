dd8102629d3166d3f3fa09ec90a1b2ac4417a73a
Roberto13-Vil <robertvilchis787@gmail.com>
2025-05-13 17:08:08 -0600

    Cambios no relevantes 11

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,18 @@
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { NotFound } from "./pages/NotFound";
 import { Toaster } from "@/components/ui/toaster";
 
 function App() {
-  console.log("✅ App.jsx se ha montado correctamente");
-  return (
-    <>
-      <Toaster />
-      <HashRouter>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </HashRouter>
-    </>
-  );
+  return<>
+    <Toaster />
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  </>
 }
 
-export default App;
+export default App
